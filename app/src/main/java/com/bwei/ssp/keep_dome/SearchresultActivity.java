b037f4b92a34c1b4d0151d044a2e0bed1720d739
songshepeng <3095842724@qq.com>
2018-02-26 10:23:19 +0800

    修改Ui

--- app/src/main/java/com/bwei/ssp/keep_dome/SearchresultActivity.java
+++ app/src/main/java/com/bwei/ssp/keep_dome/SearchresultActivity.java
@@ -22,7 +22,7 @@ public class SearchresultActivity extends AppCompatActivity {
         TextView updata_tv = (TextView) findViewById(R.id.updata_tv);
         cardview.setRadius(8);//设置图片圆角的半径大小
         tv.setText(searchtv);
-        updata_tv.setText("“"+searchtv+"”");
+        updata_tv.setText("未能搜索到“"+searchtv+"”相关的课程或体验，请尝试搜索其它运动。");
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
